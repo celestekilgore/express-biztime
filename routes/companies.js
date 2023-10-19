72aced1a49bd45fb5659a300cd8a145c2345f991
Silu Hu <erinh816@gmail.com>
2023-10-19 15:30:07 -0400

    add companies post route

--- routes/companies.js
+++ routes/companies.js
@@ -4,7 +4,7 @@ const express = require("express");
 
 const router = new express.Router();
 const db = require("../db");
-const { NotFoundError } = require("../expressError");
+const { NotFoundError, BadRequestError } = require("../expressError");
 
 /** GET / - returns `{companies: [{code,name}, ...]}` */
 router.get("/", async function (req, res, next) {
@@ -28,6 +28,20 @@ router.get("/:code", async function (req, res, next) {
   return res.json({ company });
 });
 
+/** POST / - create a company from data;
+ * takes JSON: {code, name, description}
+ * returns JSON `{company: {code, name, description}}` */
+router.post("/", async function (req, res, next) {
+  if (req.body === undefined) throw new BadRequestError();
+  const results = await db.query(
+    `INSERT INTO companies (code, name, description)
+         VALUES ($1, $2, $3)
+         RETURNING code, name, description`,
+    [req.body.code, req.body.name, req.body.description]);
+  const company = results.rows[0];
+
+  return res.status(201).json({ company });
+});
 
 
 

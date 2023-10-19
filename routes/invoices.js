adccc4de5bec4e0679fde0a98919595bb5b58c42
Silu Hu <erinh816@gmail.com>
2023-10-19 19:36:48 -0400

    add put request

--- routes/invoices.js
+++ routes/invoices.js
@@ -30,30 +30,30 @@ router.get("/", async function (req, res, next) {
 */
 router.get("/:id", async function (req, res, next) {
 
-    const id = req.params.id;
+  const id = req.params.id;
 
-    const invoiceResults = await db.query(`
+  const invoiceResults = await db.query(`
           SELECT id, amt, paid, add_date, paid_date, comp_code
           FROM invoices
           WHERE id = $1`, [id]);
 
-    const invoice = invoiceResults.rows[0];
+  const invoice = invoiceResults.rows[0];
 
-    if (!invoice) throw new NotFoundError(`No matching invoice: ${id}`);
+  if (!invoice) throw new NotFoundError(`No matching invoice: ${id}`);
 
-    company_code = invoice.comp_code;
+  company_code = invoice.comp_code;
 
-    const companyResults = await db.query(`
+  const companyResults = await db.query(`
           SELECT code, name, description
           FROM companies
           WHERE code=$1`, [company_code]);
-    const company = companyResults.rows;
+  const company = companyResults.rows;
 
-    invoice.company = company;
-    delete invoice.comp_code;
+  invoice.company = company;
+  delete invoice.comp_code;
 
-    return res.json({ invoice });
-  });
+  return res.json({ invoice });
+});
 
 
 /** POST / - create a new invoice;
@@ -77,12 +77,42 @@ router.post("/", async function (req, res, next) {
     `INSERT INTO invoices (comp_code, amt)
      VALUES ($1, $2)
      RETURNING id, comp_code, amt, paid, add_date, paid_date`,
-     [comp_code, amt]);
+    [comp_code, amt]);
 
   const invoice = results.rows[0];
 
   return res.status(201).json({ invoice });
-  
+
+});
+
+/** PUT/ [id] update an invoice
+ * take {amt}
+ * return JSON:
+ * `{invoice: {
+ * id,
+ * comp_code,
+ * amt,
+ * paid,
+ * add_date,
+ * paid_date}}`
+ */
+
+router.put("/:id", async function (req, res, next) {
+  if (req.body === undefined) throw new BadRequestError();
+
+  const id = req.params.id;
+  const amt = req.body.amt;
+
+  const results = await db.query(
+    `UPDATE invoices
+     SET amt=$1
+     WHERE id=$2
+     RETURNING id, comp_code, amt, paid, add_date, paid_date`,
+    [amt, id]);
+  const invoice = results.rows[0];
+
+  return res.json({ invoice });
+
 });
 
 
